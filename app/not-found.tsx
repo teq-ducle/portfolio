b0228c0c9b0a440626f4c2b0ff715678b0a225cd
duc <ducle@teqnological.asia>
2025-12-27 15:24:02 +0700

    chore: update navigate to /vi route when not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,23 @@
 'use client';
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import "./globals.css"
+import { useRouter } from "next/navigation";
 export default function NotFound() {
-
+  const router = useRouter()
+  // useLayoutEffect(() => {
+  //   console.log("Hi");
+    
+  //   // router.replace('/vi')
+  // }, [])
+  useEffect(() => {
+    router.replace('/vi')
+  },[])
 
   return (
-    <div className="notFound">
-      Page not found
-    </div>
+    // <div className="notFound">
+    //   Page not found
+    // </div>
+    <></>
   )
 }
 
